f92eba0807fc318b9d59e2af90f727ccd07a00c1
Yoann <yribeiro@student.42.fr>
2017-10-19 23:07:10 +0200

    itoa base

---
+++ itoa_base.c
@@ -0,0 +1,35 @@
+int		ft_pow(int nb, int pow)
+{
+	if (pow == 0)
+		return (1);
+	else
+		return (nb * ft_pow(nb, pow - 1));
+}
+
+char	*ft_itoa_base(int value, int base)
+{
+	int		i;
+	char	*nbr;
+	int		neg;
+
+	i = 1;
+	neg = 0;
+	if (value < 0)
+	{
+		if (base == 10)
+			neg = 1;
+		value *= -1;
+	}
+	while (ft_pow(base, i) - 1 < value)
+		i++;
+	nbr = (char*)malloc(sizeof(nbr) * i);
+	nbr[i + neg] = '\0';
+	while (i-- > 0)
+	{
+		nbr[i + neg] = (value % base) + (value % base > 9 ? 'A' - 10 : '0');
+		value = value / base;
+	}
+	if (neg)
+		nbr[0] = '-';
+	return (nbr);
+}
--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 int	main(void)
 {
 	int n = 10;
-
 	int *p_n = &n;
+
 	printf(   YEL "%p\n" RESET, p_n);
 	ft_printf(GRN "%p\n" RESET, p_n);
 	return (0);
