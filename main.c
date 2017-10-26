23382379c9d5f6d3c764f52fdc44ed7bda1143dd
Yoann RIBEIRO <yribeiro@e3r10p2.42.fr>
2017-10-26 19:01:42 +0200

    teub

--- main.c
+++ main.c
@@ -3,12 +3,10 @@
 int	main(void)
 {
 	int	ret;
-	int	n = 5;
-	int	*i = &n;
 
-	ret = printf("%p\n", &i);
+	ret = printf("%p\n", (void*)0);
 	//printf("\n[%d]\n", ret);
 	//printf("\n");
-	ft_printf("%p", &i);
+	ft_printf("%p", (void*)0);
 	return (0);
 }
--- process_pointer.c
+++ process_pointer.c
@@ -6,7 +6,7 @@
 /*   By: yribeiro <yribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/26 15:03:56 by yribeiro          #+#    #+#             */
-/*   Updated: 2017/10/26 18:30:57 by yribeiro         ###   ########.fr       */
+/*   Updated: 2017/10/26 18:57:12 by yribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,17 @@ int		process_pointer(t_parser *p, va_list **args)
 
 	(void)p;
 	ptr = va_arg(**args, void*);
-	if (!ptr)
-		retptr = ft_strnew(2);
-	if (ptr)
-		retptr = ft_itoa_base_u((unsigned long long)ptr, 16);
-	if (p->precision)
-	{
-		printf("1\n");
-		process_precision(&retptr, p);
-	}
-	process_htag(&retptr, p);
-	if (p->width)
-		process_width(&retptr, p);
+	retptr = ft_itoa_base_u((unsigned long long)ptr, 16);
+	ft_putstr("0x");
+	//if (!ptr)
+	//	retptr = ft_strnew(2);
+	//if (ptr)
+	//	retptr = ft_itoa_base_u((unsigned long long)ptr, 16);
+	//if (p->precision)
+	//	process_precision(&retptr, p);
+	//process_htag(&retptr, p);
+	//if (p->width)
+	//	process_width(&retptr, p);
 	ft_putstr(retptr);
 	ret = ft_strlen(retptr);
 	free(retptr);
